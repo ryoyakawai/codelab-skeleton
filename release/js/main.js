92a786d3fb001c9c5a1667c1a84e7594dbc4a6e8
Ryoya KAWAI <ryoya.kawai@gmail.com>
2015-08-02 12:29:44 +0900

    Added: WebComponentsReady

--- release/js/main.js
+++ release/js/main.js
@@ -1,5 +1,7 @@
 'use strict';
-var insert=document.querySelector("#ja").import.querySelector("template");
-document.querySelector("#steps").appendChild(
-    document.importNode(insert.content, true)
-);
+window.addEventListener('WebComponentsReady', function(event) {
+    var insert=document.querySelector("#ja").import.querySelector("template");
+    document.querySelector("#steps").appendChild(
+        document.importNode(insert.content, true)
+    );
+});
