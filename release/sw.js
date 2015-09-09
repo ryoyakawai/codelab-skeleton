accc95b14d147f4cbdd480661bd6907aa38f488f
ryoyakawai <ryoya.kawai@gmail.com>
2015-09-09 13:23:30 +0900

    Added: service worker to cache all contents

--- release/sw.js
+++ release/sw.js
@@ -1,1 +1,51 @@
 /* sw.js */
+var CACHE_NAME = 'codelabs-xwebmidi-cache';
+var urlsToCache = [
+    '/favicon.ico',
+    './step.en-us.html',
+    './step.ja-jp.html',
+    './index.html',
+    './index.html?en-us',
+    './index.html?ja-jp',
+    './manifest.json',
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+      caches.open(CACHE_NAME)
+          .then(function(cache) {
+              //console.log('Opened cache');
+              return cache.addAll(urlsToCache);
+          })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      caches.match(event.request)
+          .then(function(response) {
+              if (response) {
+                  //console.log("[return cache] ", (response.url).split("/").pop());
+                  return response;
+              }
+              var fetchRequest = event.request.clone();
+
+              return fetch(fetchRequest)
+                  .then(function(response) {
+                      if (!response || response.status !== 200 || response.type !== 'basic') {
+                          return response;
+                      }
+                      
+                      var responseToCache = response.clone();
+
+                      caches.open(CACHE_NAME)
+                          .then(function(cache) {
+                              cache.put(event.request, responseToCache);
+                          });
+                      
+                      return response;
+                  });
+          })
+  );
+});
+
